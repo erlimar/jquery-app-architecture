75d931c5869eba55dc41cb0e32f4066249b74522
Erlimar Silva Campos <erlimar@gmail.com>
2018-06-20 18:07:48 -0300

    Reorganiza arquivos

--- config.js
+++
@@ -1,9 +0,0 @@
-+function ($, App) {
-
-	var config = App.config = App.config || {}
-
-	config.app = {
-		title: 'Minha aplicação jQuery!'
-	}
-
-}(jQuery, window.ClientApp = window.ClientApp || {});
---
+++ sample/my-app.js
@@ -0,0 +1,7 @@
++function ($, App) {
+
+	var core = App.core = App.core || {}
+
+	core.setConfig('app.title', 'Minha aplicação jQuery!')
+
+}(jQuery, window.ClientApp = window.ClientApp || {});
